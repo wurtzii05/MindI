cdd3c2bee1dd262c80fb3d937dcb6a26c9fb5281
Gabriel <wurtii05@gmail.com>
2026-03-15 13:34:24 -0700

    init ui parse (rudimentary)

--- app/src/main/java/se/mindi/services/AccessibilityService.kt
+++ app/src/main/java/se/mindi/services/AccessibilityService.kt
@@ -5,7 +5,7 @@ import android.util.Log
 import android.view.KeyEvent
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
-import android.widget.Toast
+import androidx.compose.runtime.mutableStateOf
 
 
 class AccessibilityService : AccessibilityService() {
@@ -16,15 +16,37 @@ class AccessibilityService : AccessibilityService() {
 
     override fun onAccessibilityEvent(event: AccessibilityEvent) {
         // Get the source node of the event
+        if (event.eventType == 1) {
+            val source = event.source?.parent ?: return
+            val uiHierarchy = getUIString(source)
+            Log.d("ACCESS", uiHierarchy)
+        }
     }
 
     @Deprecated("Deprecated in Java")
     override fun onGesture(event: Int): Boolean {
         return false
     }
-
     protected override fun onKeyEvent(event: KeyEvent): Boolean {
-
         return super.onKeyEvent(event)
+    }
+
+    fun getUIString(node: AccessibilityNodeInfo, state: String = ""): String {
+        var s = state
+        for (i in 0 until node.childCount) {
+            try {
+                val child = node.getChild(i)
+                s = getUIString(child, s)
+                if (child.isClickable) {
+                    s += "{CLASS: ${child.className}, BUTTON: ${child.text}}\n"
+                } else {
+                    s += "{CLASS: ${child.className}, TEXT: ${child.text}}\n"
+                }
+            } catch (ex: Exception) {
+                Log.d("ERROR", "$ex")
+                return ""
+            }
+        }
+        return "$s"
     }
 }
