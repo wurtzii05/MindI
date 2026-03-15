35d0b4a7a1091e720fb85e4f029fb7e525e3c974
gabriel <wurtzii05@gmail.com>
2026-03-15 13:30:05 -0500

    tmp

--- app/src/main/java/se/mindi/services/AccessibilityService.kt
+++ app/src/main/java/se/mindi/services/AccessibilityService.kt
@@ -6,16 +6,34 @@ import android.view.KeyEvent
 import android.view.accessibility.AccessibilityEvent
 import android.view.accessibility.AccessibilityNodeInfo
 import android.widget.Toast
+import kotlin.math.log
 
 
 class AccessibilityService : AccessibilityService() {
     override fun onInterrupt() {}
     override fun onServiceConnected() {
-     
+
     }
 
     override fun onAccessibilityEvent(event: AccessibilityEvent) {
         // Get the source node of the event
+        val source = event.source ?: return
+        val parent = source.parent ?: return
+        val childcount = source.childCount
+    recursiveChild(source)
+    }
+
+    fun recursiveChild(node: AccessibilityNodeInfo) {
+        for (i in 0 until node.childCount) {
+            node.getChild(i)?.className?.let {
+                when (it.toString()) {
+                    "android.widget.TextView" -> Log.d("CHILD", node.getChild(i).toString())
+                }
+            }
+            // Log.d("CHILD", node.getChild(i).)
+            recursiveChild(node.getChild(i))
+
+        }
     }
 
     @Deprecated("Deprecated in Java")
@@ -26,5 +44,9 @@ class AccessibilityService : AccessibilityService() {
     protected override fun onKeyEvent(event: KeyEvent): Boolean {
 
         return super.onKeyEvent(event)
+    }
+
+    private fun getUITree() {
+
     }
 }
